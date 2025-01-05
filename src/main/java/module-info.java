0ec389860ea11e28c6c126a22b42b91c184984fd
traorecheikh <atidiane741@gmail.com>
2025-01-05 12:29:23 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.cheikh.gestionstock {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.cheikh.gestionstock to javafx.fxml;
+    exports com.cheikh.gestionstock;
+    exports com.cheikh.gestionstock.controllers;
+    opens com.cheikh.gestionstock.controllers to javafx.fxml;
+}
