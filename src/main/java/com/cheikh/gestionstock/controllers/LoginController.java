e5979a68c81cf0e7d724da6d8ad2dcacbc0b271a
traorecheikh <atidiane741@gmail.com>
2025-01-07 14:37:47 +0000

    some changes to the structure

--- src/main/java/com/cheikh/gestionstock/controllers/LoginController.java
+++ src/main/java/com/cheikh/gestionstock/controllers/LoginController.java
@@ -1,19 +1,41 @@
 package com.cheikh.gestionstock.controllers;
 
+import com.cheikh.gestionstock.HelloApplication;
+import com.cheikh.gestionstock.services.UserServices;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    private static UserServices us = new UserServices();
+    private Stage stage;
     @FXML
     public TextField email;
     @FXML
     public PasswordField password;
 
     @FXML
-    public void validerClick() {
+    public void validerClick() throws IOException {
+        String email = this.email.getText();
+        String password = this.password.getText();
+        if (us.Login(email,password)){
+            stage  = (Stage) this.email.getScene().getWindow();
+            HelloApplication.change(stage,"dashboard");
+            return;
+        }
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur");
+        alert.setContentText("email ou mot de passe incorrect");
     }
+
     @FXML
-    public void registerClick() {
+    public void registerClick(ActionEvent actionEvent) throws IOException {
+        stage  = (Stage) this.email.getScene().getWindow();
+        HelloApplication.change(stage,"register");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,17 @@
 module com.cheikh.gestionstock {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires jakarta.persistence;
-    requires static lombok;
     requires jdk.jfr;
+    requires static lombok;
     requires spring.security.crypto;
     requires org.slf4j;
     requires mysql.connector.j;
     requires org.hibernate.orm.core;
+    requires log4j;
     opens com.cheikh.gestionstock to javafx.fxml;
-    opens com.cheikh.gestionstock.models to org.hibernate.orm.core;
+    opens com.cheikh.gestionstock.models to org.hibernate.orm.core,javafx.fxml,javafx.base;
     exports com.cheikh.gestionstock;
     exports com.cheikh.gestionstock.controllers;
     opens com.cheikh.gestionstock.controllers to javafx.fxml;
