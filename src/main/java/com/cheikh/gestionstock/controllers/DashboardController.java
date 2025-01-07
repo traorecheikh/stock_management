4441b866e0f42c594071fef4b9357b06a2af3950
traorecheikh <atidiane741@gmail.com>
2025-01-07 14:37:14 +0000

    dashboard changes

--- src/main/java/com/cheikh/gestionstock/controllers/DashboardController.java
+++ src/main/java/com/cheikh/gestionstock/controllers/DashboardController.java
@@ -3,6 +3,7 @@ package com.cheikh.gestionstock.controllers;
 import com.cheikh.gestionstock.HelloApplication;
 import com.cheikh.gestionstock.services.UserServices;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.application.Platform;
@@ -13,8 +14,8 @@ import static com.cheikh.gestionstock.services.EntityManagerUtils.getEM;
 import static com.cheikh.gestionstock.services.UserServices.checkSession;
 
 public class DashboardController {
-
     private UserServices us = new UserServices();
+    Stage stage;
 
     @FXML
     private Label idText;
@@ -27,11 +28,24 @@ public class DashboardController {
 
                 if (checkSession() == null) {
                     getEM();
-                    HelloApplication.change(stage, "register");
+                    HelloApplication.change(stage, "login");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    @FXML
+    public void logoutClick() throws IOException {
+        us.Logout();
+        stage = (Stage) idText.getScene().getWindow();
+        HelloApplication.change(stage,"login");
+    }
+
+    @FXML
+    public void utilisateurClick() throws IOException {
+        stage = (Stage) idText.getScene().getWindow();
+        HelloApplication.change(stage,"gestion-utilisateur");
     }
 }
